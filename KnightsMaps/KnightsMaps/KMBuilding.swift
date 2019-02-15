0c1bcb7f79187284fbd70388867a86d4af8a1144
Sean <seanphall41@gmail.com>
2019-02-15 14:39:21 -0500

    Added custom location class
    added custom location class for a location object in KMBuilding

--- KnightsMaps/KnightsMaps/KMBuilding.swift
+++ KnightsMaps/KnightsMaps/KMBuilding.swift
@@ -11,16 +11,24 @@ import Foundation
 class KMBuilding {
     let name: String
     let acronym: String
-    let latitude: Float
-    let longitude: Float
+    let location: KMLocation
     let info: String
 
     init(name: String, acronym: String, latitude: Float,
          longitude: Float, info: String) {
         self.name = name
         self.acronym = acronym
+        self.location = KMLocation(latitude: latitude, longitude: longitude)
+        self.info = info
+    }
+}
+// Custom location class for storing latitude and longitude as one object
+class KMLocation {
+    let latitude: Float
+    let longitude: Float
+
+    init(latitude: Float, longitude: Float) {
         self.latitude = latitude
         self.longitude = longitude
-        self.info = info
     }
 }
