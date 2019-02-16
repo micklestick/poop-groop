1af7c94dadcde13893f2c3b148087fba7d611dfd
SeanHallKnights <seanhall@knights.ucf.edu>
2019-02-15 21:54:03 -0500

    created KMDatabaseHelper class
    created database helper class outline.

---
+++ KnightsMaps/KnightsMaps/KMDatabaseHelper.swift
@@ -0,0 +1,30 @@
+//
+//  KMDatabaseHelper.swift
+//  KnightsMaps
+//
+//  Created by Sean Hall on 2/15/19.
+//  Copyright © 2019 Alec. All rights reserved.
+//
+
+import Foundation
+
+// Class for connecting to and reading database
+class KMDatabaseHelper {
+    
+    func needUpdate() {
+        // check database for version number
+        // if version number is newer, call get() method
+    }
+    
+    func getConnection() {
+        // get connection to database to recieve JSON
+    }
+    
+    func decodeJSON() {
+        // decode JSON from database
+        
+        // store information decoded in a KMBuilding object
+        // store KMBuilding object in a KMBuilding array and return array
+    }
+    
+}
