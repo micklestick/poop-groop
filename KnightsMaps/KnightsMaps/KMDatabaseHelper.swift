7bff9f53825d287677adee76db3e628f492b596a
SeanHallKnights <seanhall@knights.ucf.edu>
2019-02-15 23:01:00 -0500

    added code to getConnection()
    added code for HTTP get request to the getConnection() function of LMDatabaseHelper

--- KnightsMaps/KnightsMaps/KMDatabaseHelper.swift
+++ KnightsMaps/KnightsMaps/KMDatabaseHelper.swift
@@ -10,21 +10,31 @@ import Foundation
 
 // Class for connecting to and reading database
 class KMDatabaseHelper {
-    
+
     func needUpdate() {
         // check database for version number
         // if version number is newer, call get() method
     }
-    
+
     func getConnection() {
         // get connection to database to recieve JSON
+        // url request with placeholder url
+        let jsonUrlString = "https://jsonplaceholder.typicode.com/users"
+        guard let url = URL(string: jsonUrlString) else { return }
+
+        // create JSON url session for get request
+        let session = URLSession.shared.dataTask(with: url) { (data, response, err) in
+            // error check here
+        }.resume()
+
+
     }
-    
+
     func decodeJSON() {
         // decode JSON from database
-        
+
         // store information decoded in a KMBuilding object
         // store KMBuilding object in a KMBuilding array and return array
     }
-    
+
 }
