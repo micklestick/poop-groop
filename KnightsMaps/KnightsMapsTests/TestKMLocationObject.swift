9326261eda543490065c0c2c2cbb56133c1631a9
Sean Hall <seanhall@knights.ucf.edu>
2019-03-02 22:50:17 -0500

    Created Test for building object
    created a test for creating a building object. added it to same test class with the location object so it runs after location

--- KnightsMaps/KnightsMapsTests/TestKMLocationObject.swift
+++ KnightsMaps/KnightsMapsTests/TestKMLocationObject.swift
@@ -12,17 +12,41 @@ import XCTest
 class TestKMLocationObject: XCTestCase {
 
     // This tests the creation of a location object using fake data
-    
+
     func testLocationCreate() {
-        
+
         let latitude: Float = 40.56
         let longitude: Float = -80.49
-        
+
         let location = KMLocation(latitude: latitude, longitude: longitude)
-        
+
         XCTAssert(location.latitude == latitude, "Latitidue created")
         XCTAssert(location.longitude == longitude, "longitude created")
 
     }
-    
+
+    // This tests the creation of a building object using fake data
+    // MUST RUN AFTER LOCATION TEST as it is dependent on the location
+    // object being created
+
+    func testBuildingCreate() {
+
+        let buildingName = "Classroom building 2"
+        let acronym = "CB2"
+        let latitude: Float = 40.56
+        let longitude: Float = -80.49
+        let info = "Test string for information field"
+
+
+        let building = KMBuilding(name: buildingName, acronym: acronym, latitude: latitude,
+                                  longitude: longitude, info: info)
+
+        XCTAssert(building.name == buildingName, "Building name")
+        XCTAssert(building.acronym == acronym, "Acronym")
+        XCTAssert(building.location.latitude == latitude, "Latitidue created")
+        XCTAssert(building.location.longitude == longitude, "longitude created")
+        XCTAssert(building.info == info, "info ")
+
+    }
+
 }
