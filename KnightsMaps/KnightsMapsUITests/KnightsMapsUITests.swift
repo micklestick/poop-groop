68bd4020a8a576eb4233321dd571179d62f809fd
Alec <ahufford92@gmail.com>
2019-03-19 19:01:22 -0400

    Tests for ViewController functions

--- KnightsMaps/KnightsMapsUITests/KnightsMapsUITests.swift
+++ KnightsMaps/KnightsMapsUITests/KnightsMapsUITests.swift
@@ -34,6 +34,40 @@ class KnightsMapsUITests: XCTestCase {
         XCUIApplication().launch()
     }
     
+    func testDegreesAndRadians(){
+        
+        let view = ViewController()
+        let rad = view.degreesToRadians(degrees: 90)
+        XCTAssert(rad == 1.5708)
+        
+        let deg = view.radiansToDegrees(radians: 1.5708)
+        XCTAssert(deg == 90.000200000000007)
+        
+    }
+    
+    func testBearingTo() {
+        let view = ViewController()
+        //location 2 should be directly south so bearing is almost -180 degrees
+        let loc1 = CLLocation(coordinate: CLLocationCoordinate2D(latitude: 28.587434545532343, longitude: -81.21362409345434), altitude: 0)
+        let loc2 = CLLocation(coordinate: CLLocationCoordinate2D(latitude: 28.588002671651058, longitude: -81.21443616420288), altitude: 0)
+        let bearingInDegrees = view.getBearingBetweenTwoPoints(point1: loc1, point2: loc2)
+        XCTAssert(bearingInDegrees == -179.69999999999999)
+        
+        
+        
+    }
+    
+    func testRounding() {
+        
+        let longDecimal = 9.432541543246435265
+        let rounded4 = longDecimal.roundTo(places: 4)
+        let rounded8 = longDecimal.roundTo(places: 8)
+        XCTAssert(rounded4 == 9.4325)
+        XCTAssert(rounded8 == 9.43254154)
+        
+        
+    }
+    
     func testDistanceLabel() {
         let app = XCUIApplication()
         let distanceLabel = app.labels["DistanceLabel"].label
