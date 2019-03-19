9241733e55a7fa3e55a6730fa1a38e4030f668cd
Alec <ahufford92@gmail.com>
2019-03-19 08:02:04 -0400

    Small fixed to code
    DBhelper - Added testing points off campus
    storyboard - fixed a conflicting constraint
    FilterView - fixed a bad dismiss

--- KnightsMaps/KnightsMaps/KMDatabaseHelper.swift
+++ KnightsMaps/KnightsMaps/KMDatabaseHelper.swift
@@ -72,12 +72,15 @@ class KMDatabaseHelper {
     
     static func aTestPoints() -> [KMBuilding] {
         
-        let testPoint1 = KMBuilding(name: "Far Building", acronym: "fb1", latitude: 28.58799939252419, longitude: -81.21444843213033, info: "Far building", type:"building")
-        
+        let testPoint1 = KMBuilding(name: "Building 1", acronym: "b1", latitude: 28.588002671651058, longitude: -81.21443616420288, info: "Far building", type:"building")
         let testPoint2 = KMBuilding(name: "Building Across", acronym: "bbb", latitude: 28.587356572418223, longitude: -81.21317020973589, info: "Building across", type:"building")
-        
         let testPoint3 = KMBuilding(name: "The Pool", acronym: "pul", latitude: 28.587804581020393, longitude: -81.21319605320281, info: "Da pool", type:"building")
-        
-        return [testPoint1, testPoint2, testPoint3]
+        let testPoint4 = KMBuilding(name: "Building 2", acronym: "b2", latitude: 28.588017643782457, longitude: -81.2140559129889, info: "Da pool", type:"building")
+        let testPoint5 = KMBuilding(name: "Building 3", acronym: "b3", latitude: 28.588235808274305, longitude: -81.2134356521542, info: "Da pool", type:"building")
+        let testPoint6 = KMBuilding(name: "Building 4", acronym: "b4", latitude: 28.58765816795291, longitude: -81.21258998104895, info: "Da pool", type:"building")
+        let testPoint7 = KMBuilding(name: "Basketball Court", acronym: "bc", latitude: 28.587876333190636, longitude: 81.21225001105631, info: "Da pool", type:"building")
+
+
+        return [testPoint1, testPoint2, testPoint3, testPoint4, testPoint5, testPoint6, testPoint7]
     }
 }
