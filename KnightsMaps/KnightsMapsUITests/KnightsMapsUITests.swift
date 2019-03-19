24332c8895315318c0608142c2622927a17091ee
Alec <ahufford92@gmail.com>
2019-03-19 18:28:17 -0400

    Tests for VC

--- KnightsMaps/KnightsMapsUITests/KnightsMapsUITests.swift
+++ KnightsMaps/KnightsMapsUITests/KnightsMapsUITests.swift
@@ -5,30 +5,56 @@
 //  Created by Alec on 2/13/19.
 //  Copyright © 2019 Alec. All rights reserved.
 //
-
+import UIKit
 import XCTest
+
+@testable import KnightsMaps
 
 class KnightsMapsUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+        //make sure our storyboard can load the different view controllers
+        let storyboard = UIStoryboard(name: "Main",
+                                      bundle: NSBundle.mainBundle())
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        viewController = navigationController.topViewController as! ViewController
+        
+        
+        UIApplication.sharedApplication().keyWindow!.rootViewController = viewController
+        
+        XCTAssertNotNil(navigationController.view)
+        XCTAssertNotNil(viewController.view)
+        // this is the line responsible for the race condition
+        NSRunLoop.mainRunLoop().runUntilDate(NSDate())
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
+    }
+    
+    func testDistanceLabel() {
+        let app = XCUIApplication()
+        let distanceLabel = app.labels["DistanceLabel"].label
+        XCTAssertEqual(distanceLabel.text(), "Distance: 0.0m")
+    }
+    
+    //test the search button
+    func testSearchButton() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateInitialViewController() as! ViewController
+        let _ = vc.view
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        //make sure the button is showing
+        XCTAssertFalse(vc.searchButton.isHidden)
+        
+        //test the button delegate
+        vc.buttonAction()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
+    
 }
